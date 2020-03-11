66247073df2486e89c26e2f55866f8d276ae43a2
Flávio Dutra <flavio.adore@gmail.com>
2020-03-11 18:06:46 -0300

    Adicionado arquivos

---
+++ casasbahia/custom.js
@@ -0,0 +1,84 @@
+function timerCountdown() {
+    $('[data-countdown]').each(function () {
+        var $this = $(this);
+        var finalDate = $(this).data('countdown');
+
+        $this.countdown(finalDate, function (event) {
+            if (!event.offset.hours && !event.offset.minutes && !event.offset.seconds) {
+                $(this).html('<span class="tfinish">Encerrado</span>');
+            } else {
+                if ($(this).attr('data-countdown-format') === 'total-hours') {
+                    var totalHours = event.offset.totalDays * 24 + event.offset.hours;
+                    totalHours = totalHours != 0 ? totalHours : '00';
+
+                    $this.html(event.strftime(''
+                    + '<span class="thour">' + totalHours + '</span>'
+                    + '<span class="tdiv">:</span><span class="tminute">%M</span>'
+                    + '<span class="tdiv">:</span><span class="tsecond">%S</span>'));
+                } else {
+                    $this.html(event.strftime(''
+                    + '<span class="tday">%D</span><span class="tdiv">:</span>'
+                    + '<span class="thour">%H</span>'
+                    + '<span class="tdiv">:</span><span class="tminute">%M</span>'
+                    + '<span class="tdiv">:</span><span class="tsecond">%S</span>'));
+                }
+            }
+        })
+        .on('finish.countdown', function () {
+            $(this).html('<span class="tfinish">Encerrado</span>');
+        });
+    });
+}
+
+$(function () {
+    if ($('[data-countdown]').length) {
+        timerCountdown();
+    }
+
+    if ($('.fade').length) {
+        $('.fade').slick({
+            infinite: true,
+            speed: 500,
+            fade: true,
+            autoplay: true,
+            autoplaySpeed: 6000,
+            cssEase: 'linear'
+        });
+    }
+    
+    $('.fade-dest').slick({
+        lazyLoad: 'ondemand',
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        slidesToShow: 5,
+        slidesToScroll: 1
+    });
+
+    
+    $('.fade-oport').slick({
+        lazyLoad: 'ondemand',
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    });
+
+    $('.fade-sazonal').slick({
+        lazyLoad: 'ondemand',
+        infinite: false,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 1
+    });
+
+
+    $('.lista-produto.prateleiral').slick({
+        lazyLoad: 'ondemand',
+        infinite: false,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 1
+    });
+});
